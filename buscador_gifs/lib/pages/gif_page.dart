eff73c70ab963a3fab775be5a4a89acb57a1ca9a
Matheus Hora <matheus11hora@gmail.com>
2022-08-31 11:57:55 -0300

    Novos

---
+++ buscador_gifs/lib/pages/gif_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_share/flutter_share.dart';
+
+
+class GifPage extends StatelessWidget {
+
+  final Map _gifData;
+
+  GifPage(this._gifData);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_gifData["title"]),
+        backgroundColor: Colors.black,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.share),
+            onPressed: (){
+
+            },
+          )
+        ],
+      ),
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Image.network(_gifData["images"]["fixed_height"]["url"]),
+      ),
+    );
+  }
+}
+
